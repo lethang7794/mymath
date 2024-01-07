bc34c886063462cc954055e575569fa36028fde1
Le Quang Thang <lethang7794@gmail.com>
2024-01-07 16:46:07 +0700

    Make Add generic

--- mymath.go
+++ mymath.go
@@ -1,11 +1,17 @@
 // Package mymath provides utilities to make it easy do math
 package mymath
 
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add returns sum of x and y.
 //
 // More information about it can be found at [Addition]
 //
 // [Addition]: https://www.mathsisfun.com/numbers/addition.html
-func Add(x, y int) int {
+func Add[T Number](x, y T) T {
 	return x + y
 }
