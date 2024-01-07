a5af75d21fe2ce4a761a95fcbe872991ba04e2a8
Le Quang Thang <lethang7794@gmail.com>
2024-01-07 16:16:16 +0700

    Add godoc comments

--- mymath.go
+++ mymath.go
@@ -1,5 +1,13 @@
+// Package mymath provides utilities to make it easy do math
 package mymath
 
+// Add does an addition operator, which bring two integer numbers together to make a new total.
+//
+// It has 2 parameters: 2 int value that needs to be brought together.
+//
+// More information about additional can be found at [Addition]
+//
+// [Addition]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
